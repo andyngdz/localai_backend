a0ce4dc5e5c1b217ef15c04ebe2dc41ee4e0d2a2
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-07-16 23:02:15 +0700

    Remove unused parameters and related code from model_loader function

--- app/model_loader/model_loader.py
+++ app/model_loader/model_loader.py
@@ -1,7 +1,4 @@
 import logging
-from multiprocessing import Queue
-from typing import Optional
-
 import torch
 from diffusers import AutoPipelineForText2Image
 
@@ -12,7 +9,7 @@ from config import BASE_CACHE_DIR
 from .schemas import MaxMemoryConfig
 logger = logging.getLogger(__name__)
 
-def model_loader(id: str, done_queue: Optional[Queue] = None):
+def model_loader(id: str):
     db = SessionLocal()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -47,10 +44,6 @@ def model_loader(id: str, done_queue: Optional[Queue] = None):
     if device == 'cuda':
         pipe.enable_model_cpu_offload()
         pipe.enable_attention_slicing()
-
-    if done_queue is not None:
-        done_queue.put(id)
-        logger.info(f'[Process] Model {id} added to done queue.')
 
     db.close()
 
