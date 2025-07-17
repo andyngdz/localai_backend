acffd2526acf742e2410311d83566314c46a806e
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-07-17 15:15:47 +0700

    Fix model downloader (#26)
    * Refactor device_map usage in model_loader to use a constant
    
    * Refactor model_loader to include model directory retrieval and database model addition
    
    * Fix logging to use DEVICE_MAP variable for model device map information

--- app/model_loader/model_loader.py
+++ app/model_loader/model_loader.py
@@ -2,8 +2,9 @@ import logging
 import torch
 from diffusers import AutoPipelineForText2Image
 
-from app.database.crud import get_selected_device
+from app.database.crud import add_model, get_selected_device
 from app.database.service import SessionLocal
+from app.services.storage import get_model_dir
 from app.socket import SocketEvents, socket_service
 
 from config import BASE_CACHE_DIR
@@ -11,6 +12,8 @@ from config import BASE_CACHE_DIR
 from .schemas import MaxMemoryConfig, DownloadCompletedResponse
 
 logger = logging.getLogger(__name__)
+
+DEVICE_MAP = "balanced"
 
 def model_loader(id: str):
     db = SessionLocal()
@@ -23,6 +26,8 @@ def model_loader(id: str):
     max_memory = MaxMemoryConfig(device, device_index).to_dict()
     logger.info(f'[Process] Setting max memory for model {id}: {max_memory}')
 
+    logger.info(f'[Process] Devide map for model {id}: {DEVICE_MAP}')
+
     try:
         pipe = AutoPipelineForText2Image.from_pretrained(
             id,
@@ -31,7 +36,7 @@ def model_loader(id: str):
             max_memory=max_memory,
             torch_dtype=torch_dtype,
             use_safetensors=True,
-            device_map="balanced",
+            device_map=DEVICE_MAP,
         )
     except EnvironmentError:
         pipe = AutoPipelineForText2Image.from_pretrained(
@@ -41,16 +46,18 @@ def model_loader(id: str):
             max_memory=max_memory,
             torch_dtype=torch_dtype,
             use_safetensors=False,
-            device_map="balanced",
+            device_map=DEVICE_MAP,
         )
 
 
     if device == 'cuda':
-        pipe.enable_model_cpu_offload()
         pipe.enable_attention_slicing()
 
-    db.close()
+    model_dir = get_model_dir(id)
+    
+    add_model(db, id, model_dir)
 
+    db.close()
 
     socket_service.emit_sync(
         SocketEvents.DOWNLOAD_COMPLETED,
