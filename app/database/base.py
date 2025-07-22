bc4d85fb6d90abc3db3de19441aba2ad86d6e0da
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-07-22 23:56:56 +0700

    fix(database): update TimestampMixin to use DateTime with timezone for created_at and updated_at fields

--- app/database/base.py
+++ app/database/base.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Any
 
-from sqlalchemy import JSON, func
+from sqlalchemy import JSON, DateTime, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -10,5 +10,7 @@ class Base(DeclarativeBase):
 
 
 class TimestampMixin:
-	created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-	updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+	created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+	updated_at: Mapped[datetime] = mapped_column(
+		DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+	)
